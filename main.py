55a084e31a18bc0474895169067b40024e042304
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-09-14 17:55:23 +0000

    Make Data Ingestion Modular

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from text_summarizer.logging import logger
+from text_summarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
 
-
-logger.info("Starting the main application...")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
