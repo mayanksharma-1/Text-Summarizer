0f9ca90080a0c77e46820a359c2387d25ae3cd78
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-09-15 22:21:28 +0000

    make data transformation modular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from text_summarizer.logging import logger
 from text_summarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.pipeline.data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
@@ -17,6 +18,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
