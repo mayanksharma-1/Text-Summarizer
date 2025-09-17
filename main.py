2d4c7f7a7c5c0dd065108a03e263b5e80e343064
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-09-17 19:29:30 +0000

    Model Evaluation module

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from text_summarizer.pipeline.data_ingestion import DataIngestionTrainingPipelin
 from text_summarizer.pipeline.data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.pipeline.data_validation import DataValidationTrainingPipeline
 from text_summarizer.pipeline.model_trainer import ModelTrainerTrainingPipeline
+from text_summarizer.pipeline.model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -42,6 +43,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
